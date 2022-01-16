9f5af7a2e415aa029c8fdf7860d70d77737761f0
Arvind Shelke <ucdonor5@gmail.com>
2022-01-17 00:07:46 +0530

    Upload model added

--- ocr_app/models.py
+++ ocr_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Upload(models.Model):
+    upload_file = models.FileField()
+    upload_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.upload_file)
+        
