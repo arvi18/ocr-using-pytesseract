effb9d0ae252843d84403fae4e90fddd6ab2a365
Arvind Shelke <ucdonor5@gmail.com>
2022-01-17 15:17:37 +0530

    refactor pdf ocr flow, updated req, added checkboxes

--- ocr_app/ocr.py
+++ ocr_app/ocr.py
@@ -1,286 +1,9 @@
 import PIL
 from PIL import ImageDraw
 
-
-def usingPytesseract(filepath):
-    img = PIL.Image.open(filepath)
-    
-
-
-
-
-
-
-
-
-import cv2
-import pytesseract
-from PIL import Image
-
-from matplotlib import pyplot as plt
-
-#https://stackoverflow.com/questions/28816046/
-#displaying-different-images-with-actual-size-in-matplotlib-subplot
-def display(im_path):
-    dpi = 80
-    im_data = plt.imread(im_path)
-
-    height, width  = im_data.shape[:2]
-    print('im_data.shape:', im_data.shape)
-    
-    # What size does the figure need to be in inches to fit the image?
-    figsize = width / float(dpi), height / float(dpi)
-
-    # Create a figure of the right size with one axes that takes up the full figure
-    fig = plt.figure(figsize=figsize)
-    ax = fig.add_axes([0, 0, 1, 1])
-
-    # Hide spines, ticks, etc.
-    ax.axis('off')
-
-    # Display the image.
-    ax.imshow(im_data, cmap='gray')
-
-    plt.show()
-
-
-
-image_file = "./data/badslide.jpg"
-img = cv2.imread(image_file)
-img=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-img=cv2.threshold(img, 210, 230, cv2.THRESH_BINARY)
-display(image_file)
-ocr=pytesseract.image_to_string(image_file)
-print('ocr:', ocr)
-
-# color = [0, 0, 0]
-# top, bottom, left, right = [50]*4
-
-# image_with_border = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)
-# ocr=pytesseract.image_to_string(image_with_border)
-# print('ocr:', ocr)
-# cv2.imwrite("temp/ocr.jpg", image_with_border)
-# display("temp/ocr.jpg")
-
-
-#https://stackoverflow.com/questions/28816046/
-#displaying-different-images-with-actual-size-in-matplotlib-subplot
-def display(im_path):
-    dpi = 80
-    im_data = plt.imread(im_path)
-
-    height, width  = im_data.shape[:2]
-    print('im_data.shape:', im_data.shape)
-    
-    # What size does the figure need to be in inches to fit the image?
-    figsize = width / float(dpi), height / float(dpi)
-
-    # Create a figure of the right size with one axes that takes up the full figure
-    fig = plt.figure(figsize=figsize)
-    ax = fig.add_axes([0, 0, 1, 1])
-
-    # Hide spines, ticks, etc.
-    ax.axis('off')
-
-    # Display the image.
-    ax.imshow(im_data, cmap='gray')
-
-    plt.show()
-display(image_file)
-image_file = "./data/badslide.jpg"
-img = cv2.imread(image_file)
-inverted_image = cv2.bitwise_not(img)
-ocr=pytesseract.image_to_string(img)
-print('ocr:', ocr)
-# print('inverted_image:', inverted_image)
-cv2.imwrite("./temp/inverted.jpg", inverted_image)
-display("./temp/inverted.jpg")
-
-def grayscale(image):
-    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-gray_image = grayscale(img)
-ocr=pytesseract.image_to_string(gray_image)
-print('ocr:', ocr)
-cv2.imwrite("temp/gray.jpg", gray_image)
-display("temp/gray.jpg")
-thresh, im_bw = cv2.threshold(gray_image, 210, 230, cv2.THRESH_BINARY)
-ocr=pytesseract.image_to_string(img)
-# print('ocr:', ocr)
-# 210, 230, 0
-cv2.imwrite("temp/bw_image.jpg", im_bw)
-display("temp/bw_image.jpg")
-def noise_removal(image):
-    import numpy as np
-    kernel = np.ones((1, 1), np.uint8)
-    image = cv2.dilate(image, kernel, iterations=1)
-    kernel = np.ones((1, 1), np.uint8)
-    image = cv2.erode(image, kernel, iterations=1)
-    image = cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel)
-    image = cv2.medianBlur(image, 3)
-    return (image)
-no_noise = noise_removal(im_bw)
-cv2.imwrite("temp/no_noise.jpg", no_noise)
-# ocr=pytesseract.image_to_string(no_noise)
-print('ocr:', ocr)
-
-display("temp/no_noise.jpg")
-
-def thin_font(image):
-    import numpy as np
-    image = cv2.bitwise_not(image)
-    kernel = np.ones((2,2),np.uint8)
-    image = cv2.erode(image, kernel, iterations=1)
-    image = cv2.bitwise_not(image)
-    return (image)
-eroded_image = thin_font(no_noise)
-ocr=pytesseract.image_to_string(eroded_image)
-print('ocr:', ocr)
-cv2.imwrite("temp/eroded_image.jpg", eroded_image)
-
-def thick_font(image):
-    import numpy as np
-    image = cv2.bitwise_not(image)
-    kernel = np.ones((2,2),np.uint8)
-    image = cv2.dilate(image, kernel, iterations=1)
-    image = cv2.bitwise_not(image)
-    return (image)
-
-
-dilated_image = thick_font(no_noise)
-ocr=pytesseract.image_to_string(dilated_image)
-print('ocr:', ocr)
-cv2.imwrite("temp/dilated_image.jpg", dilated_image)
-
-display("temp/dilated_image.jpg")
-
-# border shuold be eliminatd first
-new = cv2.imread("data/page_01_rotated.JPG")
-ocr=pytesseract.image_to_string(new)
-print('ocr:', ocr)
-display("data/page_01_rotated.JPG")
-
-#https://becominghuman.ai/how-to-automatically-deskew-straighten-a-text-image-using-opencv-a0c30aed83df
-import numpy as np
-
-def getSkewAngle(cvImage) -> float:
-    # Prep image, copy, convert to gray scale, blur, and threshold
-    newImage = cvImage.copy()
-    gray = cv2.cvtColor(newImage, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray, (9, 9), 0)
-    thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
-
-    # Apply dilate to merge text into meaningful lines/paragraphs.
-    # Use larger kernel on X axis to merge characters into single line, cancelling out any spaces.
-    # But use smaller kernel on Y axis to separate between different blocks of text
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (30, 5))
-    dilate = cv2.dilate(thresh, kernel, iterations=2)
-
-    # Find all contours
-    contours, hierarchy = cv2.findContours(dilate, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    contours = sorted(contours, key = cv2.contourArea, reverse = True)
-    for c in contours:
-        rect = cv2.boundingRect(c)
-        x,y,w,h = rect
-        cv2.rectangle(newImage,(x,y),(x+w,y+h),(0,255,0),2)
-
-    # Find largest contour and surround in min area box
-    largestContour = contours[0]
-    print (len(contours))
-    minAreaRect = cv2.minAreaRect(largestContour)
-    cv2.imwrite("temp/boxes.jpg", newImage)
-    # Determine the angle. Convert it to the value that was originally used to obtain skewed image
-    angle = minAreaRect[-1]
-    if angle < -45:
-        angle = 90 + angle
-    return -1.0 * angle
-# Rotate the image around its center
-def rotateImage(cvImage, angle: float):
-    newImage = cvImage.copy()
-    (h, w) = newImage.shape[:2]
-    center = (w // 2, h // 2)
-    M = cv2.getRotationMatrix2D(center, angle, 1.0)
-    newImage = cv2.warpAffine(newImage, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
-    return newImage
-
-
-# Deskew image
-def deskew(cvImage):
-    angle = getSkewAngle(cvImage)
-    return rotateImage(cvImage, -1.0 * angle)
-
-
-fixed = deskew(new)
-ocr=pytesseract.image_to_string(fixed)
-print('ocr:', ocr)
-cv2.imwrite("temp/rotated_fixed.jpg", fixed)
-
-
-display("temp/rotated_fixed.jpg")
-
-def remove_borders(image):
-    contours, heiarchy = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cntsSorted = sorted(contours, key=lambda x:cv2.contourArea(x))
-    cnt = cntsSorted[-1]
-    x, y, w, h = cv2.boundingRect(cnt)
-    crop = image[y:y+h, x:x+w]
-    return (crop)
-
-no_borders = remove_borders(no_noise)
-ocr=pytesseract.image_to_string(no_borders)
-print('ocr:', ocr)
-cv2.imwrite("temp/no_borders.jpg", no_borders)
-# display('temp/no_borders.jpg')
-
-color = [0, 0, 0]
-top, bottom, left, right = [50]*4
-
-
-image_with_border = cv2.copyMakeBorder(no_borders, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)
-ocr=pytesseract.image_to_string(image_with_border)
-print('ocr:', ocr)
-cv2.imwrite("temp/image_with_border.jpg", image_with_border)
-display("temp/image_with_border.jpg")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 def pdfToImg(path, pdfname):
     from pdf2image import convert_from_path
 
-    print("importinging!")
-    print("imported!")
     pages = convert_from_path(path, 500, userpw='XXX')
 
     image_counter = 1
@@ -290,7 +13,8 @@ def pdfToImg(path, pdfname):
         filepath = "media/"+pdfname+"_" + str(image_counter) + ".jpg"
         page.save(filepath, 'JPEG')
 
-        extractedText = extractedText+usingEasyOCR(filepath)
+        extractedText = extractedText+usingPytesseract(filepath)
+        # extractedText = extractedText+usingEasyOCR(filepath)
         print(extractedText)
 
         image_counter = image_counter + 1
@@ -319,3 +43,133 @@ def draw_boxes(image, bounds, color='red', width=7):
         p0, p1, p2, p3 = bound[0]
         draw.line([*p1, *p1, *p2, *p3], fill=color, width=width)
     return image
+
+
+def usingPytesseract(filepath): 
+    import cv2
+    import pytesseract
+    from PIL import Image
+    from matplotlib import pyplot as plt
+
+    def makeBorder():
+        color = [0, 0, 0]
+        top, bottom, left, right = [50]*4
+        image_with_border = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)
+        # ocr=pytesseract.image_to_string(image_with_border)
+        # print('ocr:', ocr)
+        cv2.imwrite("temp/ocr.jpg", image_with_border)
+        return image_with_border
+
+    def remove_borders(image):
+        contours, heiarchy = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cntsSorted = sorted(contours, key=lambda x:cv2.contourArea(x))
+        cnt = cntsSorted[-1]
+        x, y, w, h = cv2.boundingRect(cnt)
+        crop = image[y:y+h, x:x+w]
+        return (crop)
+
+    def grayscale(image):
+        return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    def binarize(img):
+    # thresh, im_bw = cv2.threshold(gray_img, 210, 230, cv2.THRESH_BINARY)
+        return cv2.threshold(img, 210, 230, cv2.THRESH_BINARY)
+
+    def noise_removal(image):
+        import numpy as np
+        kernel = np.ones((1, 1), np.uint8)
+        image = cv2.dilate(image, kernel, iterations=1)
+        kernel = np.ones((1, 1), np.uint8)
+        image = cv2.erode(image, kernel, iterations=1)
+        image = cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel)
+        image = cv2.medianBlur(image, 3)
+        return (image)
+     
+    def erode_(image):
+        import numpy as np
+        image = cv2.bitwise_not(image)
+        kernel = np.ones((2,2),np.uint8)
+        image = cv2.erode(image, kernel, iterations=1)
+        image = cv2.bitwise_not(image)
+        return (image)
+
+    def dilate_(image):
+        import numpy as np
+        image = cv2.bitwise_not(image)
+        kernel = np.ones((2,2),np.uint8)
+        image = cv2.dilate(image, kernel, iterations=1)
+        image = cv2.bitwise_not(image)
+        return (image)
+    
+    def deskew(cvImage):
+        angle = getSkewAngle(cvImage)
+        return rotateImage(cvImage, -1.0 * angle)
+
+    def getSkewAngle(cvImage) -> float:
+    # Prep image, copy, convert to gray scale, blur, and threshold
+        newImage = cvImage.copy()
+        gray = cv2.cvtColor(newImage, cv2.COLOR_BGR2GRAY)
+        blur = cv2.GaussianBlur(gray, (9, 9), 0)
+        thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+
+        # Apply dilate to merge text into meaningful lines/paragraphs.
+        # Use larger kernel on X axis to merge characters into single line, cancelling out any spaces.
+        # But use smaller kernel on Y axis to separate between different blocks of text
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (30, 5))
+        dilate = cv2.dilate(thresh, kernel, iterations=2)
+
+        # Find all contours
+        contours, hierarchy = cv2.findContours(dilate, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        contours = sorted(contours, key = cv2.contourArea, reverse = True)
+        for c in contours:
+            rect = cv2.boundingRect(c)
+            x,y,w,h = rect
+            cv2.rectangle(newImage,(x,y),(x+w,y+h),(0,255,0),2)
+
+        # Find largest contour and surround in min area box
+        largestContour = contours[0]
+        # print (len(contours))
+        minAreaRect = cv2.minAreaRect(largestContour)
+        # Determine the angle. Convert it to the value that was originally used to obtain skewed image
+        angle = minAreaRect[-1]
+        if angle < -45:
+            angle = 90 + angle
+        return -1.0 * angle
+    
+    def rotateImage(cvImage, angle: float):
+        newImage = cvImage.copy()
+        (h, w) = newImage.shape[:2]
+        center = (w // 2, h // 2)
+        M = cv2.getRotationMatrix2D(center, angle, 1.0)
+        newImage = cv2.warpAffine(newImage, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+        return newImage
+
+    def display(im_path):
+        dpi = 80
+        im_data = plt.imread(im_path)
+
+        height, width  = im_data.shape[:2]
+        print('im_data.shape:', im_data.shape)
+        
+        # What size does the figure need to be in inches to fit the image?
+        figsize = width / float(dpi), height / float(dpi)
+
+        # Create a figure of the right size with one axes that takes up the full figure
+        fig = plt.figure(figsize=figsize)
+        ax = fig.add_axes([0, 0, 1, 1])
+
+        # Hide spines, ticks, etc.
+        ax.axis('off')
+
+        # Display the image.
+        ax.imshow(im_data, cmap='gray')
+
+        plt.show()
+
+    img = cv2.imread(filepath)
+    gray_img = grayscale(img)
+    binarized_img = binarize(gray_img)
+    no_noise_img = noise_removal(binarized_img)
+    cv2.imwrite(filepath, no_noise_img)
+    text=pytesseract.image_to_string(filepath)
+    return text
