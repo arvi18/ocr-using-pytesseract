bd7e1beb050adde9a2465beed7ca511389b7395a
Arvind Shelke <ucdonor5@gmail.com>
2022-01-17 13:07:13 +0530

    raw pytessract method and structured easyocr function

--- ocr_app/views.py
+++ ocr_app/views.py
@@ -1,11 +1,8 @@
-from time import time
 from .models import Upload
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
-from django.http import HttpResponse
 from django.shortcuts import render
-from django.template import context
-from ocr_using_pytesseract.settings import BASE_DIR, MEDIA_ROOT
+from ocr_using_pytesseract.settings import MEDIA_ROOT
 from .models import Upload
 
 # Create your views here.
@@ -26,24 +23,15 @@ def OCRView(req):
 
         if file.name.endswith(".pdf"):
             isPDF = True
-            file_instance=Upload.objects.create(upload_file=file)
-            file_url=MEDIA_ROOT+"\\"+str(file_instance)
+
+            file_instance = Upload.objects.create(upload_file=file)
+            file_url = MEDIA_ROOT+"\\"+str(file_instance)
+
             from .ocr import pdfToImg
-            text=pdfToImg(file_url, str(file_instance))
+            text = pdfToImg(file_url, str(file_instance))
         else:
             isPDF = False
     else:
         isPDF = False
-        text=None
-    return render(req, "ocr.html", {'isPDF': isPDF, 'text':text})
-
-
-class UploadView(CreateView):
-    model = Upload
-    fields = ['upload_file', ]
-    success_url = reverse_lazy('fileupload')
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['documents'] = Upload.objects.all()
-        return context
+        text = None
+    return render(req, "ocr.html", {'isPDF': isPDF, 'text': text})
