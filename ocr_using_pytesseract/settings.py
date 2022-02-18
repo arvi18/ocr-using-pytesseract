f28c1412cf8a23d7c7d088e3d1379a6631d38ea3
Arvind Shelke <ucdonor5@gmail.com>
2022-02-18 14:43:57 +0530

    heroku //

--- ocr_using_pytesseract/settings.py
+++ ocr_using_pytesseract/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
-import django_heroku
+# import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -136,6 +136,6 @@ FILE_UPLOAD_TEMP_DIR = '/tmp'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
